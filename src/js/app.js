91f6cade54d79e5103331bda8cbe1f07902fbd23
Vasjak08 <vasjak0818@gmail.com>
2026-05-31 23:14:22 +0300

    7771

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 // Підключення функціоналу "Чортоги Фрілансера"
-//import "../styles/style.scss";
-//import "../styles/fonts/iconfont.css";
+import "../styles/style.scss";
+import "../styles/fonts/iconfont.css";
 import { addTouchAttr, addLoadedAttr, isMobile, FLS } from "@js/common/functions.js"
